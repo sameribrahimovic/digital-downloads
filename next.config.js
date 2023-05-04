cc52fbf11dc8dbab9310f997d022bd6eebff5117
Samer Ibrahimovic <sameribrahimovic@hotmail.com>
2023-05-04 15:34:59 +0200

    Initial project setup

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost',
+      'bootcamp-flavio.s3.eu-central-1.amazonaws.com',
+      'bootcamp-flavio.s3.amazonaws.com',
+    ],
+  },
+}
